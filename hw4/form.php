61c37a33bfc7f90630af53cce4fbbe0c73578455
Nurzhan Sagyndyk <38518770+NakeSaga@users.noreply.github.com>
2019-10-09 12:40:46 +0600

    Add files via upload

---
+++ hw4/form.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+
+		<form action="welcome.php" method="post">
+			<p>Name: </p> <input type="text" name="name"><br>
+			<p>Surname: </p>
+			<input type="text" name="surname"><br>
+			<input type="submit"><br>
+		</form>
+
+	</body>
+</html>
---
+++ hw4/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+Hi my name is <?php
+	        echo $_POST["name"];
+               ?> <br>
+and surname is <?php
+                echo $_POST["surname"];
+               ?>
+</body>
+
+</html>
